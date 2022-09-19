3566d3b067a399bcb3e18962ccb347b77b016e04
Rafalk88 <99990595+Rafalk88@users.noreply.github.com>
2022-09-19 15:39:40 +0200

    task 2 done

--- 02/components/App.js
+++ 02/components/App.js
@@ -2,15 +2,22 @@
 import React from 'react';
 import Box from './Box';
 
+import { TextContent } from '../context';
+
 class App extends React.Component {
     state = {
         text: 'React HelloWorld Modern!',
     };
 
     render() {
+        const { Provider } = TextContent;
         const { text } = this.state;
 
-        return <Box text={text} />;
+        return (
+            <Provider value={text}>
+                <Box />
+            </Provider>
+        );
     }
 }
 
---
+++ 02/context.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const TextContent = React.createContext();
+
+export default TextContent;
